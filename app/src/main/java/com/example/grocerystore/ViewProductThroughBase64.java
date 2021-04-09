7aa4b7051b600fe81cfddfe62965ad02ff469a7c
Pragati <pragatigawade5355@gmail.com>
2021-04-09 09:52:07 +0530

    grocerystore

---
+++ app/src/main/java/com/example/grocerystore/ViewProductThroughBase64.java
@@ -0,0 +1,23 @@
+package com.example.grocerystore;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ViewProductThroughBase64 extends Fragment {
+
+    CircleImageView circleImageView;
+    EditText name,price,brand,discount;
+
+
+
+}
